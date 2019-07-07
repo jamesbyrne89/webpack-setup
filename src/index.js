53f81fddbfd2420575beb08c2a143710d24c38be
jamesbyrne89 <mail@jamestbyrne.com>
2019-07-07 20:59:22 +0100

    Add ESLint

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { render } from 'react-dom';
 console.log('Welcome to my webpack setup');
 
 const MyComponent = () => {
-  return <div>This is a React component</div>;
+  return <div>This is a React component!</div>;
 };
 
 render(<MyComponent />, document.getElementById('root'));
