dadc05c8eb271603d297036abf9bc2155758ff5d
jamesbyrne89 <mail@jamestbyrne.com>
2019-07-07 18:04:06 +0100

    Add React component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './test';
+import React from 'react';
+import { render } from 'react-dom';
 
 console.log('Welcome to my webpack setup');
+
+const MyComponent = () => {
+  return <div>This is a React component</div>;
+};
+
+render(<MyComponent />, document.getElementById('root'));
